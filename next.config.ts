2315bc1381f5d3b2678e51773c2d19e124ebe6e6
Nadhif Ibrahim <mnadhifibr@gmail.com>
2026-06-27 04:53:36 +0700

    fix: hardcode backend IP directly in next config to bypass vercel env issues

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*/',
-        destination: `${process.env.BACKEND_URL || 'http://localhost:8000/api'}/:path*/`
+        destination: 'http://44.194.104.133/api/:path*/'
       },
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL || 'http://localhost:8000/api'}/:path*/`
+        destination: 'http://44.194.104.133/api/:path*/'
       }
     ];
   }
