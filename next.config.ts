5f36cfb8b8becc1697d4db4e211ee7780e864fdb
Nadhif Ibrahim <mnadhifibr@gmail.com>
2026-06-27 05:05:17 +0700

    apaansi

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: true,
   async rewrites() {
     return [
       {
-        source: '/api/:path*/',
-        destination: 'http://44.194.104.133/api/:path*/'
-      },
-      {
         source: '/api/:path*',
-        destination: 'http://44.194.104.133/api/:path*/'
+        destination: 'http://44.194.104.133/api/:path*'
       }
     ];
   }
